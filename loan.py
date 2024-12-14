571f9c02233915c45ef0d1bde1d717e647783952
Mert Muslu <electraonam@hotmail.com>
2024-12-14 18:41:20 +0300

    ufak düzeltme

--- loan.py
+++ loan.py
@@ -10,21 +10,21 @@ dataset = pd.read_csv("loan_data.csv", header= 0)
 loan_status_yes = dataset[dataset['loan_status'] == 1]
 loan_status_no = dataset[dataset['loan_status'] == 0]
 
+loan_status_yes = loan_status_yes.reset_index(drop=True)
+loan_status_no = loan_status_no.reset_index(drop=True)
 
 #print("Column names:", list(dataset.columns))
+num_of_yes = loan_status_yes.index.tolist()
+num_of_no = loan_status_no.index.tolist()
 
-num_of_yes = loan_status_yes.shape[0]
-num_of_no = loan_status_no.shape[0]
+#Number of loan status = 1 indices (10000)
+train_yes_index, temp_yes_index = train_test_split(num_of_yes, test_size=0.3, random_state=42)
+test_yes_index, validation_yes_index = train_test_split(temp_yes_index, test_size = 2/3, random_state= 42)
 
-#Number of loan status = 1 indices (35000)
-yes_indices = list(range(num_of_yes))
-train_yes_index, test_yes_index = train_test_split(yes_indices, test_size=0.2, random_state=42)
-train_yes_index, validation_yes_index = train_test_split(train_yes_index, test_size = 1/8, random_state= 42)
+#Number of loan status = 0 indices (35000)
+train_no_index, temp_no_index = train_test_split(num_of_no, test_size=0.3, random_state=42)
+test_no_index, validation_no_index = train_test_split(temp_no_index, test_size = 2/3, random_state= 42)
 
-#Number of loan status = 0 indices (10000)
-no_indices = list(range(num_of_no))
-train_no_index, test_no_index = train_test_split(no_indices, test_size=0.2, random_state=42)
-train_no_index, validation_no_index = train_test_split(train_no_index, test_size = 1/8, random_state= 42)
 
 #get appropriate row numbers from dataset according to the indices
 #for loan_status = 1
@@ -37,9 +37,8 @@ train_no = loan_status_no.iloc[train_no_index]
 validation_no = loan_status_no.iloc[validation_no_index]
 test_no = loan_status_no.iloc[test_no_index]
 
+
 #merging operations
 train_data = pd.concat([train_yes, train_no],ignore_index = True)
 validation_data = pd.concat([validation_yes, validation_no],ignore_index = True)
 test_data = pd.concat([test_yes, test_no],ignore_index = True)
-
-print(len(train_data))
